7168d10626babb3e505e0a74da21d9bb099a967d
TechnoGeek01 <rsudharsangames@gmail.com>
2023-07-27 15:34:23 +0530

    “added pagination, sorting and advanced regex filters”

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,67 @@
 const Product = require("../models/product.js");
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
 
-  const products = await Product.find(queryObject);
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      "<": "$lt",
+      ">=": "$gte",
+      "<=": "$lte",
+      "=": "$eq",
+    };
+    const regex = /\b(<|>|>=|<=|=)\b/g;
+    let filters = numericFilters.replace(regex, (match) => {
+      return `-${operatorMap[match]}-`;
+    });
+
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
+  let result = Product.find(queryObject);
+
+  // sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  // returns only the required fields from products
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,5 @@ const { getAllProducts } = require("../controllers/products.js");
 const router = express.Router();
 
 router.route("/").get(getAllProducts);
-router.route("/static").get(getAllProductsStatic);
 
 module.exports = router;
